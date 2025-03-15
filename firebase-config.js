470436a24d70b6229ca7763b380ae0a966ac8e56
angelfa06 <124756773+angelfa06@users.noreply.github.com>
2025-03-15 18:53:37 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,21 @@
-// Configuración de Firebase
-// IMPORTANTE: Reemplaza estos valores con los que obtengas de tu consola de Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "tu-proyecto.firebaseapp.com",
-    projectId: "tu-proyecto",
-    storageBucket: "tu-proyecto.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef123456"
-  };
-  
-  // Inicializar Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Exportar las referencias a usar en otros archivos
-  const db = firebase.firestore();
-  const productsCollection = db.collection('products');
+  apiKey: "AIzaSyDjZWHSipApN4kC3mLYzUSAhM8METCr-kM",
+  authDomain: "lr-textiles.firebaseapp.com",
+  projectId: "lr-textiles",
+  storageBucket: "lr-textiles.firebasestorage.app",
+  messagingSenderId: "961739289163",
+  appId: "1:961739289163:web:9d20ced5cdde63981fd3ce",
+  measurementId: "G-RMCXBZH2JS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
